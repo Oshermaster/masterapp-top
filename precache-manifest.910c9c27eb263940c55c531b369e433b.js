01b1754a2363f09f820e26c0538789ae6062738c
drimix001 <69410679+drimix001@users.noreply.github.com>
2020-08-12 16:49:48 +0300

    Updates

---
+++ precache-manifest.910c9c27eb263940c55c531b369e433b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "718d1c632b6e622fd740fc35eee5aa4a",
+    "url": "/masterapp-top/index.html"
+  },
+  {
+    "revision": "dd550daea5a5e209f103",
+    "url": "/masterapp-top/static/css/main.4baa8a09.chunk.css"
+  },
+  {
+    "revision": "aa70f188fecd673b6efe",
+    "url": "/masterapp-top/static/js/2.2b57bea6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/masterapp-top/static/js/2.2b57bea6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd550daea5a5e209f103",
+    "url": "/masterapp-top/static/js/main.8006eafc.chunk.js"
+  },
+  {
+    "revision": "455e0042af35225387e4",
+    "url": "/masterapp-top/static/js/runtime-main.e401b0a2.js"
+  }
+]);
